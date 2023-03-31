6db81d4b2c0e18566c0b8353b36167736c3ee77d
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-03-31 21:49:49 +0000

    removed react query

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
-import { QueryClientProvider, QueryClient } from 'react-query'
 import App from './App'
-
-const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
     <ChakraProvider>
     <App />
     </ChakraProvider>
-    </QueryClientProvider>
   </React.StrictMode>,
 )
